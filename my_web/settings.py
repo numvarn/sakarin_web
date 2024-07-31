74fd706aefb7ce201238ec33cb234ab08444696f
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2024-07-31 17:31:09 +0700

    config to deploy

--- my_web/settings.py
+++ my_web/settings.py
@@ -76,10 +76,10 @@ WSGI_APPLICATION = "my_web.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
 }
 
 
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'statics', ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
