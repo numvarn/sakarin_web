2970c57db5093e95b8697963853a6d1649d9c243
ItzSakkarinTH <itzsakkarinthyt@gmail.com>
2024-07-31 13:22:34 +0700

    setup webpage

--- webpage/urls.py
+++ webpage/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.indexPage),
-    path("about/", views.aboutPage),
-    path("contact/", views.contactPage),
+    path("", views.indexPage, name="index"),
+    path("about/", views.aboutPage, name="about"),
+    path("contact/", views.contactPage, name="contact"),
 
 ]
